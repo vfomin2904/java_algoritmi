fce1cec871aea5decde70ebd3c0c5a2065a42255
vfomin2904 <vova-fomin97@mail.ru>
2021-07-17 18:05:14 +0300

    lesson

---
+++ hw7/src/DepthFirstPath.java
@@ -0,0 +1,43 @@
+package lesson7;
+
+import java.util.LinkedList;
+
+public class DepthFirstPath {
+    private boolean[] marked;
+    private int[] edgeTo;
+    private int source;
+
+    public DepthFirstPath(Graph g, int source) {
+        this.source = source;
+        marked = new boolean[g.getVertexCount()];
+        edgeTo = new int[g.getVertexCount()];
+        dfs(g, source);
+    }
+
+    private void dfs(Graph g, int v) {
+        marked[v] = true;
+        for (int w : g.getAdjList(v)) {
+            if (!marked[w]) {
+                edgeTo[w] = v;
+                dfs(g, w);
+            }
+        }
+    }
+
+    public boolean hasPathTo(int v) {
+        return marked[v];
+    }
+
+    public LinkedList<Integer> pathTo(int v) {
+        if (!hasPathTo(v)) {
+            return null;
+        }
+        LinkedList<Integer> stack = new LinkedList<>();
+        int vertex = v;
+        while (vertex != source){
+            stack.push(vertex);
+            vertex = edgeTo[vertex];
+        }
+        return stack;
+    }
+}
